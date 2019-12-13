989487e22c65dec736ee98174ae02fb9dc7d49c4
yamolean <moririnmanro9608@gmail.com>
2019-12-13 16:43:42 +0900

    add WebView

--- WebView+RxSwift/AppDelegate.swift
+++ WebView+RxSwift/AppDelegate.swift
@@ -11,10 +11,15 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController =
+            UINavigationController(rootViewController: ViewController())
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
         return true
     }
 
