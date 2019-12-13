dd69c03b6e18c760e92714e5d72447117a795aa7
yamolean <moririnmanro9608@gmail.com>
2019-12-13 16:48:07 +0900

    Refactor

--- WebView+RxSwift/ViewController.swift
+++ WebView+RxSwift/ViewController.swift
@@ -22,34 +22,36 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        let loadingObservable = webView.rx.loading.share()
-        
-        // プログレスバーの表示・非表示
-        loadingObservable
-            .map { return !$0 }
-            .observeOn(MainScheduler.instance)
-            .bind(to: progressView.rx.isHidden)
-            .disposed(by: disposeBag)
-        // iPhone の上部の時計のところのバーの(名称不明)アクティビティインジケータ表示制
-        loadingObservable
-            .bind(to: UIApplication.shared.rx.isNetworkActivityIndicatorVisible)
-            .disposed(by: disposeBag)
-        // NavigationController のタイトル表示
-        webView.rx.title
-            .filterNil()
-            .observeOn(MainScheduler.instance)
-            .bind(to: navigationItem.rx.title)
-            .disposed(by: disposeBag)
-        // プログレスバーのゲージ制御
-        webView.rx.estimatedProgress
-            .map { return Float($0) }
-            .observeOn(MainScheduler.instance)
-            .bind(to: progressView.rx.progress)
-            .disposed(by: disposeBag)
-        let url = URL(string: "https://www.google.com/")
-        let urlRequest = URLRequest(url: url!)
-        webView.load(urlRequest)
+        setupWebView()
     }
+    
+    fileprivate func setupWebView() {
+           let loadingObservable = webView.rx.loading.share()
+           // プログレスバーの表示・非表示
+           loadingObservable
+               .map { return !$0 }
+               .observeOn(MainScheduler.instance)
+               .bind(to: progressView.rx.isHidden)
+               .disposed(by: disposeBag)
+           // iPhone の上部の時計のところのバーの(名称不明)アクティビティインジケータ表示制
+           loadingObservable
+               .bind(to: UIApplication.shared.rx.isNetworkActivityIndicatorVisible)
+               .disposed(by: disposeBag)
+           // NavigationController のタイトル表示
+           webView.rx.title
+               .filterNil()
+               .observeOn(MainScheduler.instance)
+               .bind(to: navigationItem.rx.title)
+               .disposed(by: disposeBag)
+           // プログレスバーのゲージ制御
+           webView.rx.estimatedProgress
+               .map { return Float($0) }
+               .observeOn(MainScheduler.instance)
+               .bind(to: progressView.rx.progress)
+               .disposed(by: disposeBag)
+           let url = URL(string: "https://www.google.com/")
+           let urlRequest = URLRequest(url: url!)
+           webView.load(urlRequest)
+       }
 }
 
